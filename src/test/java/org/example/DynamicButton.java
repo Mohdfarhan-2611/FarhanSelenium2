1abd21dc01ba847309e533c12062112ddc170127
user\\mhdfa <mhdfarhan2611@gmail.com>
2025-05-31 01:01:57 +0530

    Initial commit

--- src/test/java/org/example/DynamicButton.java
+++ src/test/java/org/example/DynamicButton.java
@@ -1,6 +1,28 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
 public class DynamicButton {
 
+    @Test
+    public void ClickToggleButton() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        WebElement button = driver.findElement(By.xpath("//button[@onclick='toggleButton(this)']"));
+        for(int i=1; i<=10; i++){
+            button.click();
+            Thread.sleep(2000);
+        }
+
+
+
+    }
 
 }
