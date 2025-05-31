c6cc9439b63bb5d03dd334569c57357dd2f72126
user\\mhdfa <mhdfarhan2611@gmail.com>
2025-05-31 21:37:05 +0530

    Initial commit

---
+++ src/test/java/org/example/UploadFilesMultiple.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UploadFilesMultiple {
+
+    @Test
+    public void UploadFileExample() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+
+
+
+
+
+
+
+    }
+}
